362a43921022783463b1f0ba0ae6d795779f6fef
Winnie1003 <winnie_wangqy@yahoo.com>
2017-07-06 21:22:16 +0800

    add a file

---
+++ printName.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+
+#1. 使用input获取必要的信息
+name = raw_input("Please input your name:")
+QQ = raw_input("Please input your QQ number:")
+
+#2. 使用print来打印名片
+print ( "=====================")
+print("NAME:%s"%name)
+print("QQ:%s"%QQ)
+print("=====================")
+
+
